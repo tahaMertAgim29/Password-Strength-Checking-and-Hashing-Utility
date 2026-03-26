95f79b93de883ed3739443406ecc2bbcfd4ca481
Taha Mert Ağım <157625541+tahaMertAgim29@users.noreply.github.com>
2026-03-26 15:33:53 +0300

    Add files via upload

---
+++ Cyber_Security/config.py
@@ -0,0 +1,27 @@
+#*
+# Configuration file
+# Password Policy Parameters &
+# Hashing Parameters are set from here
+# It can be changed or modified from here
+#
+# *#
+
+# Policy Parameters (Changeable from here)
+POLICY_CONFIG = {
+    "min_length": 12, # That means it must be at least 12 character long
+    "min_uppercase": 1, # That means it must contain at least one uppercase character
+    "min_symbol": 1, # That means it must contain at least one symbol
+    "strong_entropy_threshold": 80.0,
+    "moderate_entropy_threshold": 50.0,
+}
+
+# Hashing Parameters (Bcrypt and Argon2 hashes are used. These parameters can also be modified)
+HASHING_CONFIG = {
+    "bcrypt_rounds": 12,  # Cost Factor: 2^12 (4096) rounds
+    "argon2_time_cost": 2,
+    "argon2_memory_cost": 65536, # Memory-hard parameter
+    "argon2_parallelism": 4,
+}
+
+
+DICTIONARY_FILE = "dictionary.txt" # The text file of dictionary where compromised or weak passwords are involved in
